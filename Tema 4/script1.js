7536fae766d557e5e5f3ab3dcf482620e050ff19
edgar-wilches <edgar.wilches.ttc.933@unilibre.edu.co>
2024-12-03 18:39:33 -0500

    Bucle añadido

--- Tema 4/script1.js
+++ Tema 4/script1.js
@@ -1,34 +1,46 @@
-let a = parseFloat(prompt("Ingrese el primer número"));
-let b = parseFloat(prompt("Ingrese el segundo número"));
-let operacion = prompt("Ingrese la operación (+ , - , * , / ):");
+// Bucle para realizar múltiples operaciones
+while (true) {
+  let a = parseFloat(prompt("Ingrese el primer número"));
+  let b = parseFloat(prompt("Ingrese el segundo número"));
+  let operacion = prompt(
+    "Ingrese la operación (+ , - , * , / ) o 'salir' para terminar:"
+  );
 
-//Función calculadora
-function realizarOperacion(num1, num2, operacion) {
-  if (isNaN(num1) || isNaN(num2)) {
-    console.log("Error: Uno de los valores ingresados no es válido");
-    return "Entrada no válida"; //Verifica si las entradas están vacías
+  // Salir del bucle si el usuario escribe 'salir'
+  if (operacion.toLocaleLowerCase() === "salir") {
+    console.log("Has salido de la calculadora.");
+    document.write("\nHas salido de la calculadora.");
+    break;
   }
-  //Operaciones
-  if (operacion == "+") {
-    return num1 + num2;
-  } else if (operacion == "-") {
-    return num1 - num2;
-  } else if (operacion == "*") {
-    return num1 * num2;
-  } else if (operacion == "/") {
-    if (num2 == 0) {
-      console.log("Error: División por 0.");
-      return "Error: División por cero";
-    } else {
-      return num1 / num2;
+
+  // Función calculadora
+  function realizarOperacion(num1, num2, operacion) {
+    if (isNaN(num1) || isNaN(num2)) {
+      console.log("Error: Uno de los valores ingresados no es válido");
+      return "Entrada no válida"; // Verifica si las entradas están vacías
     }
-  } else {
-    console.log("Error: Operación no válida");
-    return "Operación no válida";
+    // Operaciones
+    if (operacion == "+") {
+      return num1 + num2;
+    } else if (operacion == "-") {
+      return num1 - num2;
+    } else if (operacion == "*") {
+      return num1 * num2;
+    } else if (operacion == "/") {
+      if (num2 == 0) {
+        console.log("Error: División por 0.");
+        return "Error: División por cero";
+      } else {
+        return num1 / num2;
+      }
+    } else {
+      console.log("Error: Operación no válida");
+      return "Operación no válida";
+    }
   }
+
+  let resultado = realizarOperacion(a, b, operacion);
+
+  console.log("El resultado es: " + resultado);
+  document.write("El resultado es: " + resultado + "<br>");
 }
-
-let resultado = realizarOperacion(a, b, operacion);
-
-console.log("El resultado es: " + resultado);
-document.write("El resultado es: " + resultado);
